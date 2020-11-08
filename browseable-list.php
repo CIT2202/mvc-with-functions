1b5f7ab9c94d2db0fa0a6adba4e304b50ab3a8a6
M Mantle <mmantlehud@googlemail.com>
2020-11-08 13:37:04 +0000

    Fixed browseable-list

--- browseable-list.php
+++ browseable-list.php
@@ -1,38 +1,6 @@
 <?php
-try{
-    $conn = new PDO('mysql:host=localhost;dbname=cit2202', 'cit2202', 'letmein');
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch (PDOException $exception)
-{
-	echo "Oh no, there was a problem" . $exception->getMessage();
-}
-
-//select all the films
-$query = "SELECT * FROM films";
-$resultset = $conn->query($query);
-$films = $resultset->fetchAll();
-$conn=NULL;
-
+include "models/film-model.php";
+$films = getAllFilms();
+$title = "List all films";
+include "views/list-view.php";
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>List the films</title>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-</head>
-<body>
-	<ul>
-<?php
-
-//loop over the array of films
-foreach ($films as $film) {
-    echo "<li>";
-    echo "<a href='details.php?id={$film["id"]}'>{$film["title"]}</a>";
-    echo "</li>";
-}
-
-?>
-</ul>
-</body>
-</html>
